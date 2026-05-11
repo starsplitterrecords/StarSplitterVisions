a251ee8d76cb9d00d56476f67eb5881027540e0b
starsplitterrecords <77013062+starsplitterrecords@users.noreply.github.com>
2026-05-11 13:01:32 -0400

    STA-162: add Azure Reach cover to JS asset registry

--- src/data/assets.js
+++ src/data/assets.js
@@ -6,4 +6,5 @@ export const brandAssets = {
 
 export const coverAssets = {
   vikingsIssue01: '/images/covers/vikings-2026-issue-01.jpg',
+  azureReachIssue01: '/images/covers/azure-reach-issue-01-cover.jpg',
 }
