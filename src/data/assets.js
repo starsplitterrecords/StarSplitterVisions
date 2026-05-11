020a31bea4c0e656f177be326087b13ab81308da
starsplitterrecords <77013062+starsplitterrecords@users.noreply.github.com>
2026-05-11 10:12:47 -0400

    STA-74: restore missing asset registry

---
+++ src/data/assets.js
@@ -0,0 +1,9 @@
+export const brandAssets = {
+  hero: '/images/brand/hero.png',
+  logo: '/images/brand/logo.png',
+  icon: '/images/brand/icon.png',
+}
+
+export const coverAssets = {
+  vikingsIssue01: '/images/covers/vikings-2026-issue-01.jpg',
+}
